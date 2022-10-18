8d5c28061c8bf24d21ce9b8b9f37bdc96c02b719
Shyama Menon <shyama12@hotmail.com>
2022-10-18 23:41:15 +0200

    Restaurants and reviews

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :restaurants, only: %i[index new create show] do
+    # remove new for reviews when the form is in the restaurants/show page
+    resources :reviews, only: %i[new create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts 'Seeding restaurants'
+
+Restaurant.destroy_all
+categories = %w[chinese italian japanese french belgian]
+
+10.times do
+  restaurant = Restaurant.create(name: Faker::Restaurant.name,
+                                 address: Faker::Address.street_address,
+                                 phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+                                 category: categories.sample)
+
+  puts 'Creating reviews'
+  reviews_count = rand(0..4)
+  reviews_count.times do
+    review = Review.create(rating: rand(0..5), content: Faker::Restaurant.review, restaurant: restaurant)
+    puts "Created review with id #{review.id}"
+  end
+
+  puts 'Finished creating reviews'
+  puts "Created restaurant with id #{restaurant.id}"
+end
+
+puts 'Finished creating restaurants'
